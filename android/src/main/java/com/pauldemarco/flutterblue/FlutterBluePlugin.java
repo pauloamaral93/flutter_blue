259a2714157b56e30d8969416254161590bf9cad
Paulo Amaral <pauloamaral93@hotmail.com>
2019-10-04 14:15:43 +0100

    600 mtu

--- android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
@@ -839,7 +839,7 @@ public class FlutterBluePlugin implements MethodCallHandler, RequestPermissionsR
                 / Changes the MTU size to 512 in case LOLLIPOP and above devices
                 */
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    exchangeGattMtu(500, gatt);
+                    exchangeGattMtu(600, gatt);
                 }
                 
                 servicesDiscoveredSink.success(p.build().toByteArray());
