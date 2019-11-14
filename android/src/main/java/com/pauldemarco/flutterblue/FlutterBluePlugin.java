0ba0082ea33b85cccadd8a3701fdeb8b335747a8
Paulo Amaral <pauloamaral93@hotmail.com>
2019-11-14 11:50:32 +0000

    fix

--- android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
+++ android/src/main/java/com/pauldemarco/flutterblue/FlutterBluePlugin.java
@@ -838,24 +838,24 @@ public class FlutterBluePlugin implements MethodCallHandler, RequestPermissionsR
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             log(LogLevel.DEBUG, "[onServicesDiscovered] count: " + gatt.getServices().size() + " status: " + status);
-          
-		    
-		    registrar.activity().runOnUiThread(
+		
+		 registrar.activity().runOnUiThread(
                 new Runnable() {
                     @Override
                     public void run() {
+                        
 			    if(servicesDiscoveredSink != null) {
-                Protos.DiscoverServicesResult.Builder p = Protos.DiscoverServicesResult.newBuilder();
-                p.setRemoteId(gatt.getDevice().getAddress());
-                for(BluetoothGattService s : gatt.getServices()) {
-                    p.addServices(ProtoMaker.from(gatt.getDevice(), s, gatt));
-                }
-				    
-                        servicesDiscoveredSink.success(p.build().toByteArray());
+				Protos.DiscoverServicesResult.Builder p = Protos.DiscoverServicesResult.newBuilder();
+				p.setRemoteId(gatt.getDevice().getAddress());
+				for(BluetoothGattService s : gatt.getServices()) {
+				    p.addServices(ProtoMaker.from(gatt.getDevice(), s, gatt));
+				}
+				servicesDiscoveredSink.success(p.build().toByteArray());
+			    }
+			    
                     }
                 });
-                
-            }
+             
         }
         
         private void exchangeGattMtu(int mtu, BluetoothGatt gatt) {
