9ece89d1d2fbcb7fe09fdc571359c5ef2533aab7
Paulo Amaral <pauloamaral93@hotmail.com>
2020-02-13 15:12:22 +0000

    Update bluetooth_device.dart

--- lib/src/bluetooth_device.dart
+++ lib/src/bluetooth_device.dart
@@ -52,7 +52,7 @@ class BluetoothDevice {
   }
   
   Future<bool> getBondState(int size) async {
-    return await FlutterBle.instance._channel.invokeMethod("getDeviceBondState", {
+    return await FlutterBlue.instance._channel.invokeMethod("getDeviceBondState", {
       "size": size,
       "remoteId": id.toString(),
     });
